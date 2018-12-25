aeca2d2158859c52f627dd57365ab140da8146a4
Анна Белоусова <yanuta2000@mail.ru>
2018-12-25 18:41:24 +0600

    1st commit

---
+++ src/lab3/Lab3.java
@@ -0,0 +1,31 @@
+
+package lab3;
+
+import java.util.Scanner;
+
+
+public class Lab3 {
+
+
+         public static void main(String[] args){
+        Circle circle = new Circle();
+        Triangle triangle = new Triangle();
+        Rectangle rectangle = new Rectangle();
+
+        Scanner scan = new Scanner(System.in);
+
+        for(;;) {
+            System.out.println("Введите название желаемой фигуры на русском языке с большой буквы"+"\nВведите пробел для выхода");
+            String name = scan.nextLine();
+            //System.out.println(name);
+            switch (name){
+                case "Треугольник": triangle.Draw(); break;
+                case "Круг": circle.Draw(); break;
+                case "Прямоугольник": rectangle.Draw(); break;
+                case " ": System.exit(0);
+                default: System.out.println("Некорректный ввод, повторите попытку");
+            }
+        }
+    }
+    }
+    
---
+++ src/lab3/Shape.java
@@ -0,0 +1,7 @@
+package lab3;
+
+public abstract class Shape {
+
+    public abstract void Draw();
+
+}
